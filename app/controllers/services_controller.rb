5e95efe02be581e78f6b20c178c68653b5c5e282
Poyraz <poyrazg19@hotmai.com>
2025-10-09 17:30:03 -0500

    Added a filter by type search function to services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,6 +1,12 @@
 class ServicesController < ApplicationController
   def index
+    @search = params[:search].to_s.strip
     @services = Service.order(:name).page(params[:page]).per(10)
+
+    if @search.present?
+    query = "%#{@search}%"
+    @services = @services.where("(name) LIKE ? ", query)
+    end
   end
 
   def show
