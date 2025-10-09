79a73b0aef81b4b9d0b092a8d9cfaa0a99304aed
Poyraz <poyrazg19@hotmai.com>
2025-10-09 17:07:58 -0500

    Add hierarchical filter to facilities by city

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -1,6 +1,12 @@
 class FacilitiesController < ApplicationController
   def index
-    @facilities = Facility.order(:name).page(params[:page]).per(10)
+    @cities  = City.order(:name)
+    @city_id = params[:city_id].presence
+
+    scope = Facility.order(:name)
+    scope = scope.where(city_id: @city_id) if @city_id
+
+    @facilities = scope.page(params[:page]).per(10)
   end
 
   def show
